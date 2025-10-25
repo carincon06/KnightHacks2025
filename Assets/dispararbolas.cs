7314f6be30265afa1ea303fef2447570e3838636
Alejandro <alecoco09j@gmail.com>
2025-10-25 11:29:19 -0400

    add ball script

---
+++ Assets/dispararbolas.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class dispararbolas : MonoBehaviour
+{
+    public Transform mano;
+    public int time;
+    public int force;
+    private Vector3 direction;
+    public void SetDirection(Vector3 newDirection)
+    {
+        direction = newDirection.normalized; // Ensure it's a unit vector
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetDirection(mano.forward);
+
+        Destroy(gameObject, time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += direction * Time.deltaTime * force;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, transform.forward * 2);
+    }
+}
