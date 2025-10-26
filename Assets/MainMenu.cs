be04e38eb9d069c7caa5e857f45dd593acafa50b
Empanalisa <luciana.elisalazo@gmail.com>
2025-10-25 22:40:55 -0400

    scenes changes

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Loads your main game scene
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    // Goes back to the main menu
+    public void LoadMainMenu()
+    {
+        Debug.Log("LoadMainMenu pressed");
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    // Loads the "You Win" scene
+    public void LoadWinScreen()
+    {
+        SceneManager.LoadSceneAsync("YouWinScreen");
+    }
+
+    // Loads the "You Lose" scene
+    public void LoadLoseScreen()
+    {
+        SceneManager.LoadSceneAsync("YouLoseScreen");
+    }
+
+}
